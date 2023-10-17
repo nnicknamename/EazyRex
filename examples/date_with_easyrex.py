c00bf374c559f05a19e6f268184f25b779e4310a
Ait hellal Noureddine <65350100+nnicknamename@users.noreply.github.com>
2023-10-17 19:33:57 +0200

    Update date_with_easyrex.py

--- examples/date_with_easyrex.py
+++ examples/date_with_easyrex.py
@@ -1,15 +1,14 @@
 from Easyrex import *
 
 
-# To create a regular expression for a date with Easyrex, we can use one of the following approaches:
+# To create a regular expression for a date with Easyrex,
+# we can use one of the following approaches:
 if __name__=="__main__":
-
-    
     # using the provided base regular expression and the addition operator.
     #   here the addition represents a concatination.
     date=Utils.digits+" "+Utils.word+" "+Utils.digits
     print(date)
 
     # we can do the same thing using custom expressions and even a mix of strings and expressions
-    date=Expression("\d+")+" \w+ "+Utils.digits
+    date=Expression(r"\d+")+r" \w+ "+Utils.digits
     print(date)
